8112348cff950f6edf32dd4ddc390b24c06cfa8f
wamaione <166937967+wamaione@users.noreply.github.com>
2024-04-25 23:35:44 +0200

    Add files via upload

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { FaMinusCircle } from 'react-icons/fa';
 import { FaPlusCircle } from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -38,6 +39,7 @@ const ExpenseItem = (props) => {
         <td>{currency}{props.cost}</td>
         <td><FaPlusCircle size='2.2em' color="green" onClick={handleAdd10}></FaPlusCircle></td>
         <td><FaMinusCircle size='2.2em' color="red" onClick={handleSubtract10}></FaMinusCircle></td>
+        <td><AiFillCloseCircle size='2.2em' color="black" onClick={handleSubtract10}></AiFillCloseCircle></td>
        
         
         </tr>
